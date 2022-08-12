79e6155dbfacc2838ce73b7f21b0410e00f2d3ad
Jesús Gutierrez <jgutierrezyancan@outlook.com>
2022-08-12 16:49:37 -0500

    back hecho, soporta get profiles y post profiles

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
+const DB_HOST = require('../env')
 const express = require("express")
 const mongoose = require("mongoose")
 const routes = require("./routes")
 const Profile = require("./models/profile")
-const HOST = 'mongodb+srv://jesusAdmin:alameda@cluster0.qxfijzy.mongodb.net/?retryWrites=true&w=majority';
 
 mongoose
-	.connect(HOST, { useNewUrlParser: true })
+	.connect(DB_HOST, { useNewUrlParser: true })
 	.then(() => {
 		const app = express()
     app.use(express.json())
